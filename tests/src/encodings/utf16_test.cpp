23577067b67133a852a35f49fbcf24ca95613ff5
osdever <krasota156@gmail.com>
2023-01-12 01:54:41 +0300

    [tests.encodings] Add UTF encoding tests

--- tests/src/encodings/utf16_test.cpp
+++ tests/src/encodings/utf16_test.cpp
@@ -8,20 +8,20 @@
 
 #include <gtest/gtest.h>
 
-TEST(Utf8Test, ToUtf16)
+TEST(Utf16Test, ToUtf8)
 {
-    constexpr ulib::u8char kUtf8Str[] = u8"Hello world! 😎";
     constexpr ulib::u16char kUtf16Str[] = u"Hello world! 😎";
+    constexpr ulib::u8char kUtf8Str[] = u8"Hello world! 😎";
 
-    const auto converted = ulib::Convert<ulib::Utf8, ulib::Utf16>(kUtf8Str);    
-    ASSERT_EQ(converted, kUtf16Str);
+    const auto converted = ulib::Convert<ulib::Utf16, ulib::Utf8>(kUtf16Str);    
+    ASSERT_EQ(converted, kUtf8Str);
 }
 
-TEST(Utf8Test, ToUtf32)
+TEST(Utf16Test, ToUtf32)
 {
-    constexpr ulib::u8char kUtf8Str[] = u8"Hello world! 😎";
+    constexpr ulib::u16char kUtf16Str[] = u"Hello world! 😎";
     constexpr ulib::u32char kUtf32Str[] = U"Hello world! 😎";
 
-    const auto converted = ulib::Convert<ulib::Utf8, ulib::Utf32>(kUtf8Str);    
+    const auto converted = ulib::Convert<ulib::Utf16, ulib::Utf32>(kUtf16Str);    
     ASSERT_EQ(converted, kUtf32Str);
 }
---
+++ tests/src/encodings/utf32_test.cpp
@@ -0,0 +1,27 @@
+#include <ulib/encodings/utf8/encoding.h>
+#include <ulib/encodings/utf16/encoding.h>
+#include <ulib/encodings/utf32/encoding.h>
+
+#include <ulib/encodings/converter.h>
+
+#include <ulib/containers/string_ostream.h>
+
+#include <gtest/gtest.h>
+
+TEST(Utf32Test, ToUtf8)
+{
+    constexpr ulib::u32char kUtf32Str[] = U"Hello world! 😎";
+    constexpr ulib::u8char kUtf8Str[] = u8"Hello world! 😎";
+
+    const auto converted = ulib::Convert<ulib::Utf32, ulib::Utf8>(kUtf32Str);    
+    ASSERT_EQ(converted, kUtf8Str);
+}
+
+TEST(Utf32Test, ToUtf16)
+{
+    constexpr ulib::u32char kUtf32Str[] = U"Hello world! 😎";
+    constexpr ulib::u16char kUtf16Str[] = u"Hello world! 😎";
+
+    const auto converted = ulib::Convert<ulib::Utf32, ulib::Utf16>(kUtf32Str);    
+    ASSERT_EQ(converted, kUtf16Str);
+}
