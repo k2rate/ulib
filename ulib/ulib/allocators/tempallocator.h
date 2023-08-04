cf952a90c16b6b94044775091ef71744974c6fde
k2re8 <dns.1983n@gmail.com>
2023-08-04 18:12:15 +0300

    [tempallocator] make static

--- ulib/ulib/allocators/tempallocator.h
+++ ulib/ulib/allocators/tempallocator.h
@@ -6,10 +6,11 @@
 // #include <ulib/allocators/mallocallocator.h>
 
 #include <ulib/allocators/defaultallocator.h>
+#include <ulib/allocators/staticallocator.h>
 
 namespace ulib
 {
     struct TempAllocatorTagT {};
     // using TempAllocatorT = ulib::StaticAllocator<ulib::SynchronizedAllocator<ulib::FastMemAllocator<ulib::MallocAllocator>>, TempAllocatorTagT>;
-    using TempAllocatorT = DefaultAllocator;
+    using TempAllocatorT = StaticAllocator<DefaultAllocator, TempAllocatorTagT>;
 }
